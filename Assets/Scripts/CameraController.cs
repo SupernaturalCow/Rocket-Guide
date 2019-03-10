1d8a8a6a3ebc26976adff7e6ee542c6164bca92a
SupernaturalCow <inaniloquaint@gmail.com>
2019-03-10 20:18:07 +1000

    <Working> Added the last touch of polish.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] float followDistance;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var cameraX = transform.position.x;
+        var playerX = GameObject.Find("Player").transform.position.x;
+        if (Mathf.Abs(playerX - cameraX) > followDistance)
+        {
+            cameraX += (playerX - cameraX) * 0.001f;
+        }
+        transform.position = new Vector3(cameraX, transform.position.y, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] public AudioClip gameStart;
+    [SerializeField] public AudioClip gameNextLevel;
+    [SerializeField] public AudioClip gameRestart;
+    [SerializeField] public AudioClip gameMusic;
+
+    public AudioSource audioSource;
+
+    public static GameController i;
+    void Awake()
+    {
+        BePersistent();
+        audioSource = GetComponent<AudioSource>();
+        audioSource.PlayOneShot(gameMusic);
+    }
+
+    private void BePersistent()
+    {
+        if (!i)
+        {
+            i = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            //Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        SetUpGame();
+    }
+
+    private void SetUpGame()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            var intro = GameObject.Find("IntroString");
+            if (intro != null)
+            {
+                Destroy(intro);
+                audioSource.PlayOneShot(gameStart);
+            }
+        }
+    }
+}
